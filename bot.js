6b2d5ba8d8f362bb5cfe4414dffafd773ecde41a
joakandr <joakandr@users.noreply.github.com>
2017-03-25 23:29:49 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function respond() {
       botRegex = /^\/google\s[\s\S]+$/;
 
   if(request.text && botRegex.test(request.text)) {
-    var search_string = request.text.substring(8);
+    var search_string = request.text.substring(8).replace(' ', '+');
     this.res.writeHead(200);
     postMessage(search_string);
     this.res.end();
